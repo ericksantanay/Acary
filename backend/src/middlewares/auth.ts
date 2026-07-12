297d3a07381449396ba1785d77e221091cc17932
Erick Silva <donizttierick@gmail.com>
2026-07-12 16:52:32 -0300

    Auth finalizado

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -1,8 +1,35 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import prisma from "../lib/prisma";
 // Para rotas privadas
 
-export function verificarToken(req: Request, res: Response, next: NextFunction) {
-    
+type ID = {
+    id: string
 }
+
+interface RequestUserId extends Request {
+    userId?: string
+}
+
+export function verificarToken(req: RequestUserId, res: Response, next: NextFunction) {
+
+    // Pegando o acess Token
+    const accessToken = req.cookies.acess_token
+
+     // Verificando se veio pelo req
+    if (!accessToken) {
+         return res.status(401).json({mensagem: "Acesso negado. Token não fornecido."});
+    }
+
+    try {
+
+         // Valida o token. Se falhar, o código pula direto para o bloco catch. 
+        const {id} = jwt.verify(accessToken, process.env.JWT_SECRET ?? '') as ID
+
+        req.userId = id
+
+        // Next (Proximo)
+        next()
+    } catch (error) {
+        return res.status(401).json({ error: 'Token inválido ou expirado' });
+    };
+};
