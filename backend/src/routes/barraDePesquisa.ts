980670edd229ddf484874231aa5cbe637d99cac5
Erick Silva <donizttierick@gmail.com>
2026-07-14 16:28:19 -0300

    Barra de Pesquisa ajustes

--- backend/src/routes/barraDePesquisa.ts
+++ backend/src/routes/barraDePesquisa.ts
@@ -25,11 +25,11 @@ router.get("/pesquisa", async (req: Request, res: Response) => {
             return res.status(404).json({mensagem: "Cidade não encontrada"});
         };
 
-        return res.status(200).json(buscaUsuarioBaba)
+        return res.status(200).json(buscaUsuarioBaba);
 
     } catch (error) {
-        return res.status(500).json({mensagem: "Erro no servidor"})
-    }
+        return res.status(500).json({mensagem: "Erro no servidor"});
+    };
     
 })
 
