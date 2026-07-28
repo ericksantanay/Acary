e2934dd0baff3f0328424000b07c36b9bd622128
Erick Silva <donizttierick@gmail.com>
2026-07-28 17:22:25 -0300

    Finalizado

--- frontend/src/features/paginaInicialFamilia/postagemDeServicos.ts
+++ frontend/src/features/paginaInicialFamilia/postagemDeServicos.ts
@@ -27,6 +27,9 @@ if (formularioPostagem) {
                 return alert("Preencha os campos corretamente");
             };
 
+            const valorValueEmNumber = parseInt(valorValue)
+            const criancasValueEmNumber = parseInt(criancasValue);
+
             if (Number.isNaN(valorValue)) {
                 return alert("O campo de valor deve conter apenas números válidos.");
             };
@@ -37,18 +40,45 @@ if (formularioPostagem) {
                 const URL = "https://backend-acary.onrender.com";
 
                 fetch(`${URL}/postarServicos`, {
-                    method: "POST";
-                    
+                    method: "POST",
+                    credentials: "include", // Cookie
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        cidade: cidadeValue,
+                        criancas: criancasValueEmNumber,
+                        valor: valorValueEmNumber,
+                        responsavel: responsavelValue,
+                        dataEhorarioInicio: dataInicioValue,
+                        dataEhorarioTermino: dataFinalValue
+                    })
                 })
+                .then((res) => res.json())
+                .then((dados) => {
+                    
+                    console.log(dados);
 
+                    if (dados.mensagem === "Valor inválido") {
+                        return alert("Valor inválido");
+                    };
 
+                    if (dados.mensagem === "Numero de crianças inválido") {
+                        return alert("Numero de crianças inválido");
+                    };
 
+                    if (dados.mensagem === "Esse serviço já existe.") {
+                        return alert("Esse serviço já existe.");
+                    };
+
+                    if (dados.mensagem === "Serviço criado com sucesso.") {
+                        return alert("Serviço criado com sucesso.");
+                    };
+                })
 
             } catch (error) {
                 return alert("Erro no servidor, tente novamente mais tarde");
             };
-
-        }
-
+        };
     });
 };
