380738a2e62af966d19227bf5b8ae6ed610152b5
Erick Silva <donizttierick@gmail.com>
2026-07-28 18:20:07 -0300

    Finalizado com ajustes

---
+++ frontend/dist/features/login/login.js
@@ -0,0 +1,95 @@
+"use strict";
+const formFamilias = document.getElementById('formulario-login-familia');
+const formBabas = document.getElementById('formulario-login-baba');
+// Formulario familia
+if (formFamilias) {
+    formFamilias.addEventListener("submit", (impedirFormFamilia) => {
+        impedirFormFamilia.preventDefault();
+        // Inputs
+        const emailFamilia = document.getElementById('email');
+        const senhaFamilia = document.getElementById('senha');
+        if (emailFamilia instanceof HTMLInputElement && senhaFamilia instanceof HTMLInputElement) {
+            const emailValue = emailFamilia.value.trim();
+            const senhaValue = senhaFamilia.value.trim();
+            if (!emailValue || !senhaValue) {
+                return alert("Preencha os campos corretamente");
+            }
+            ;
+            const URL = "https://backend-acary.onrender.com";
+            fetch(`${URL}/loginUsuario`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email: emailValue,
+                    senha: senhaValue
+                })
+            })
+                .then((res) => res.json())
+                .then((dados) => {
+                console.log(dados);
+                if (dados.mensagem === "Usuário ou senha incorretos") {
+                    return alert("Usuário ou senha incorretos");
+                }
+                ;
+                if (dados.mensagem === "Login efetuado com sucesso") {
+                    alert("Login efetuado com sucesso");
+                    return window.location.replace("http://127.0.0.1:5500/frontend/src/features/paginaInicialFamilia/paginaInicialFamilia.html");
+                }
+                ;
+            }).catch((error) => {
+                console.log(error);
+                return alert("Erro no servidor");
+            });
+        }
+        ;
+    });
+}
+;
+// Formulario Babas
+if (formBabas) {
+    formBabas.addEventListener("submit", (impedirFormBaba) => {
+        impedirFormBaba.preventDefault();
+        const emailBaba = document.getElementById('email-baba');
+        const senhaBaba = document.getElementById('senha-baba');
+        if (emailBaba instanceof HTMLInputElement && senhaBaba instanceof HTMLInputElement) {
+            const emailBabaValue = emailBaba.value.trim();
+            const senhaBabaValue = senhaBaba.value.trim();
+            if (!emailBabaValue || !senhaBabaValue) {
+                return alert("Preencha os campos corretamente");
+            }
+            ;
+            const URLBaba = "https://backend-acary.onrender.com";
+            fetch(`${URLBaba}/loginUsuario`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email: emailBaba,
+                    senha: senhaBaba
+                })
+            })
+                .then((res) => res.json())
+                .then((dados) => {
+                console.log(dados);
+                if (dados.mensagem === "Usuário ou senha incorretos") {
+                    return alert("Usuário ou senha incorretos");
+                }
+                ;
+                if (dados.mensagem === "Login efetuado com sucesso") {
+                    alert("Login efetuado com sucesso");
+                    return location.replace("http://127.0.0.1:5500/frontend/src/features/paginaBaba/paginaBaba.html");
+                }
+            }).catch((error) => {
+                console.log("Baba" + error);
+                return alert("Erro no servidor");
+            });
+        }
+        ;
+    });
+}
+;
